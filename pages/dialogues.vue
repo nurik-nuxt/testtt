da6e26bea591d53f5759816a7b04783c5d93c424
nurtuganulyn <nurtuganulyn@gmail.com>
2024-05-28 14:10:28 +0500

    feat:dialogs

--- pages/dialogues.vue
+++ pages/dialogues.vue
@@ -1,7 +1,121 @@
 <script setup lang="ts">
-console.log('Dialogues');
+const { t } = useI18n();
+
+const items = ref([
+  {
+    id: 1,
+    label: t('searchAllBots')
+  },
+  {
+    id: 2,
+    label: 'Бот 1'
+  },
+  {
+    id: 3,
+    label: 'Бот '
+  }
+])
 </script>
 
 <template>
-  <div>Dialogues</div>
+  <div class="grid">
+    <div class="col-12">
+      <div class="card dialogue-wrapper">
+        <div class="flex w-full h-full">
+          <div class="user-list h-full">
+            <div class="flex align-items-center gap-2 mb-3">
+              <IconField class="w-full" iconPosition="left">
+                <InputText  type="text" :placeholder="t('searchBase')" class="w-full" style="padding-top: 1rem; padding-bottom: 1rem; font-size: 16px" />
+                <InputIcon class="pi pi-search" style="font-size: 16px" />
+              </IconField>
+              <SplitButton icon="pi pi-check" menuButtonIcon="pi pi-sliders-h" :model="items" />
+
+<!--              <i style="cursor: pointer; font-size: 22px; color: #1E63CE" class="pi pi-sliders-h" />-->
+            </div>
+            <span>
+              {{ $t('dialogsFound') }}: 1
+            </span>
+            <div class="user-card mt-3">
+              <div class="avatar-wrapper gap-2">
+                <Avatar label="A" class="mr-2" size="large" shape="circle" />
+                <div>
+                  <div class="text-white">Alexander</div>
+                  <div style="color: #D0C4C6">куку</div>
+                </div>
+              </div>
+              <div class="text-white">пн</div>
+            </div>
+          </div>
+          <div class="w-full flex flex-column chat-wrapper">
+            <div class="chat-header">
+              <div class="avatar-wrapper">
+                <Avatar label="A" class="mr-2" size="large" shape="circle" />
+                <div>
+                  <div>Alexander</div>
+                  <a target="_blank" href="https://t.me/systemsalex_bot/gift" style="color: #076AE1;">https://t.me/systemsalex_bot/gift</a>
+                </div>
+              </div>
+              <div class="text-lg">
+                {{ $t('pauseBot')}}
+                <i style="cursor: pointer; font-size: 18px; margin-left: 8px; color: #1E63CE" class="pi pi-pause" />
+              </div>
+            </div>
+            <div class="chat-content">
+              chat-content
+            </div>
+            <div class="chat-message">
+              <Textarea type="text" id="message" class="w-full" :placeholder="t('enterMessage')" :autoResize="true" rows="1" cols="2" />
+              <i style="cursor: pointer; font-size: 18px;" class="pi pi-send" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.dialogue-wrapper {
+  height: calc(100vh - 9rem);
+  padding: 0;
+}
+.user-list {
+  width: 500px;
+  padding: 1rem;
+  border-right: 1px solid var(--surface-border);
+}
+.chat-wrapper {
+  background: #F8F8F8;
+}
+.chat-header {
+  padding: 1rem;
+  background: var(--surface-card);
+  box-shadow: var(--card-shadow);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.chat-message {
+  margin-top: auto;
+  padding: 1rem;
+  background: var(--surface-card);
+  box-shadow: var(--card-shadow);
+  display: flex;
+  align-items: center;
+  gap: 8px;
+}
+.user-card {
+  background: #175CCA;
+  padding: 12px 8px;
+  border-radius: 12px;
+  display: flex;
+  justify-content: space-between;
+}
+:deep(.p-splitbutton .p-splitbutton-defaultbutton){
+  display: none;
+}
+:deep(.p-splitbutton-menubutton) {
+  border-radius: 6px;
+  font-size: 18px;
+}
+</style>
