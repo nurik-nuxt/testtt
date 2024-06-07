72aaa5ef6494517acabf26bb659c0e209ea13244
nurtuganulyn <nurtuganulyn@gmail.com>
2024-06-07 19:42:58 +0500

    fix: admin knowledge

--- pages/admin/knowledge/create.vue
+++ pages/admin/knowledge/create.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts">
-console.log('Knowledge Create');
 const route = useRoute();
+const { t } = useI18n();
+const value = ref('');
+import Editor from 'primevue/editor';
+
 </script>
 
 <template>
-  <div>
-    Create Knowledge for Chatbot {{ route.query.chatbot }}
+  <div class="grid">
+    <div class="col-12">
+      <div class="card h-full">
+        <h5 class="font-bold">{{ $t('createKnowledgeTitle') }} {{ route.query.chatbot }}</h5>
+        <Editor v-model="value" editorStyle="height: 320px" />
+        <div class="mt-4 flex gap-4 align-items-center justify-content-end">
+          <nuxt-link to="/chatbots" style="color: #334155">{{ $t('goBack')}}</nuxt-link>
+          <Button :label="t('createFile')"></Button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
--- pages/admin/knowledge/index.vue
+++ pages/admin/knowledge/index.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-console.log('Admin knowledge');
 
 const bots = ref([
   {
