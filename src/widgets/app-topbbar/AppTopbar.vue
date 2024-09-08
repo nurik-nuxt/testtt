ba285e7956a47d05e3f07c0e5b38de10f6cffa84
nurtuganulyn <nurtuganulyn@gmail.com>
2024-09-08 13:14:52 +0500

    fix

--- src/widgets/app-topbbar/AppTopbar.vue
+++ src/widgets/app-topbbar/AppTopbar.vue
@@ -68,6 +68,20 @@ const applyImpersonate = async () => {
 
 <template>
   <div class="layout-topbar">
+<!--    <nuxt-link v-if="authStore.isAdmin" to="/" class="layout-topbar-logo">-->
+<!--      <span class="layout-topbar-logo-text">Admin</span>-->
+<!--    </nuxt-link>-->
+<!--    <div v-else-if="authStore.isSupport" class="flex align-items-center gap-4">-->
+<!--      <span>{{ $t('idClient') }}:</span>-->
+<!--      <InputText type="text" v-model="userIdToImpersonate" />-->
+<!--      <Button :disabled="!userIdToImpersonate.length" :label="t('apply')" @click="applyImpersonate"/>-->
+<!--      <span style="color: red" v-if="authStore.supportedUserEmail">{{ authStore.supportedUserEmail }}</span>-->
+<!--    </div>-->
+
+    <button v-if="!authStore.isSupport" class="p-link layout-menu-button layout-topbar-button" @click="onMenuToggle()">
+      <i class="pi pi-bars"></i>
+    </button>
+
     <nuxt-link v-if="authStore.isAdmin" to="/" class="layout-topbar-logo">
       <span class="layout-topbar-logo-text">Admin</span>
     </nuxt-link>
@@ -82,9 +96,6 @@ const applyImpersonate = async () => {
     </nuxt-link>
 
 
-    <button v-if="!authStore.isSupport" class="p-link layout-menu-button layout-topbar-button" @click="onMenuToggle()">
-      <i class="pi pi-bars"></i>
-    </button>
 
     <button v-if="!chatVisible && route?.name ==='chatbots-id'" style="margin-left: auto" class="mobile-chat-toggle p-link layout-topbar-menu-button layout-topbar-button" @click="setToggleChat(true)">
       <i class="pi pi-comments"></i>
